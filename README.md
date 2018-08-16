ad2e466b1245576ef94ae1bc462d9b7f677a16bb
Allen, Timothy <tallen@wharton.upenn.edu>
2018-08-16 17:42:48 +0000

    Initial commit of project skeleton.

---
+++ README.md
@@ -0,0 +1,22 @@
+# Django CyberSource Hosted Checkout
+
+This package provides utilities for using CyberSource Secure Acceptance Hosted Checkout.
+
+## Settings
+
+* CYBERSOURCE_URL
+    * For testing / development: 'https://testsecureacceptance.cybersource.com/pay'
+    * For production: 'https://secureacceptance.cybersource.com/pay'
+CYBERSOURCE_PROFILE_ID = '[Your CyberSource Profile ID]'
+CYBERSOURCE_ACCESS_KEY = '[Your CyberSource Access Key]'
+CYBERSOURCE_SECRET_KEY = '[Your CyberSource Secret Key'
+
+## Release Notes
+
+### 0.1.0
+
+* Initial release
+
+## Contributors
+
+* Timothy Allen (https://github.com/FlipperPA)
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(
+    name='django-cybersource-hosted-checkout',
+    version="0.1.0",
+    description='This package provides utilities for using CyberSource Secure Acceptance Hosted Checkout.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Tim Allen',
+    author_email='tallen@wharton.upenn.edu',
+    url='https://github.com/FlipperPA/django-ssh-deployer',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        'Django',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
