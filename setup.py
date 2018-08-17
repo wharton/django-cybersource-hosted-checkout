4732a27e898ad293b22fd2d0f242b520ae246cf6
Allen, Timothy <tallen@wharton.upenn.edu>
2018-08-17 20:21:37 +0000

    Bump version number.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='django-cybersource-hosted-checkout',
-    version="0.0.2",
+    version="0.0.3",
     description='This package provides utilities for using CyberSource Secure Acceptance Hosted Checkout.',
     long_description=long_description,
     long_description_content_type='text/markdown',
